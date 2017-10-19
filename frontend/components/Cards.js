7065bcf6db7ecac06567a1c505349c021bbe84d5
Divyendu Singh <divyendu.z@gmail.com>
2017-10-19 14:49:30 +0530

    frontend: simplify query

--- frontend/components/Cards.js
+++ frontend/components/Cards.js
@@ -34,8 +34,8 @@ const styles = {
 };
 
 const allCards = gql`
-  query allCards($first: Int!, $skip: Int!) {
-    allCards(orderBy: title_ASC, first: $first, skip: $skip) {
+  query allCards {
+    allCards(orderBy: title_ASC) {
       id
       title
       link
@@ -46,34 +46,11 @@ const allCards = gql`
   }
 `;
 
-const POSTS_PER_PAGE = 100;
-
-// The `graphql` wrapper executes a GraphQL query and makes the results
-// available on the `data` prop of the wrapped component (PostList)
 export default graphql(allCards, {
   options: {
-    variables: {
-      skip: 0,
-      first: POSTS_PER_PAGE
-    }
+    variables: {}
   },
   props: ({ data }) => ({
-    data,
-    loadMorePosts: () => {
-      return data.fetchMore({
-        variables: {
-          skip: data.allCards.length
-        },
-        updateQuery: (previousResult, { fetchMoreResult }) => {
-          if (!fetchMoreResult) {
-            return previousResult;
-          }
-          return Object.assign({}, previousResult, {
-            // Append the new posts results to the old one
-            allCards: [...previousResult.allCards, ...fetchMoreResult.allCards]
-          });
-        }
-      });
-    }
+    data
   })
 })(Cards);

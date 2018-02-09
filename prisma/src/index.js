d2bffae431f819aca9c2a119cd1608b86c784e84
Divyendu Singh <divyendu.z@gmail.com>
2018-02-09 17:10:09 +0530

    prisma: equivalent of graphcool baas

---
+++ prisma/src/index.js
@@ -0,0 +1,101 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
+
+const resolvers = {
+  Query: {
+    cards(
+      parent,
+      { where, orderBy, skip, after, before, first, last },
+      ctx,
+      info
+    ) {
+      return ctx.db.query.cards(
+        {
+          where,
+          orderBy,
+          skip,
+          after,
+          before,
+          first,
+          last
+        },
+        info
+      );
+    },
+    user(parent, { where }, ctx, info) {
+      return ctx.db.query.user(
+        {
+          where
+        },
+        info
+      );
+    }
+  },
+  Mutation: {
+    createCard(parent, { data }, ctx, info) {
+      return ctx.db.mutation.createCard(
+        {
+          data
+        },
+        info
+      );
+    },
+    updateCard(parent, { where, data }, ctx, info) {
+      return ctx.db.mutation.updateCard(
+        {
+          where,
+          data
+        },
+        info
+      );
+    },
+    deleteCard(parent, { where }, ctx, info) {
+      return ctx.db.mutation.deleteCard(
+        {
+          where
+        },
+        info
+      );
+    },
+
+    createUser(parent, { data }, ctx, info) {
+      return ctx.db.mutation.createUser({
+        data
+      });
+    },
+    updateUser(parent, { where, data }, ctx, info) {
+      return ctx.db.mutation.updateUser(
+        {
+          where,
+          data
+        },
+        info
+      );
+    },
+    deleteUser(parent, { where }, ctx, info) {
+      return ctx.db.mutation.deleteUser(
+        {
+          where
+        },
+        info
+      );
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint: "http://localhost:4466/ds/dev", // the endpoint of the Prisma DB service
+      secret:
+        "very-secret-going-to-version-control-but-i-know-it-and-dont-care", // specified in database/prisma.yml
+      debug: false // log all GraphQL queryies & mutations
+    })
+  })
+});
+
+server.start(() => console.log("Server is running on http://localhost:4000"));
